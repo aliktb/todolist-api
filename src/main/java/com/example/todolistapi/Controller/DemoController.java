322baec8c99a426c8877b972072cb58d325e79bb
Ali <aliktb@zohomail.eu>
2022-09-10 14:07:18 +0100

    added inital pring boot application

---
+++ src/main/java/com/example/todolistapi/Controller/DemoController.java
@@ -0,0 +1,22 @@
+package com.example.todolistapi.Controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping("/demo")
+public class DemoController {
+
+    @GetMapping("/test")
+    public ResponseEntity<String> TestEndpoint(){
+
+        log.info("Fetching demo/test endpoint");
+        return new ResponseEntity<String>("Running!", HttpStatus.OK);
+    }
+
+}
